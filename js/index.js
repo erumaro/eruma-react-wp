59d4e60094bf939b5dcfd14ddef7ceb02f168110
Tobias Årud <me@tobiasarud.se>
2017-02-16 14:10:01 +0100

    forgot to delete files, used wrong git command to add them...

--- js/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { Router, browserHistory } from 'react-router';
